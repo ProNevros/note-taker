45ad76a5dc378ef3d9cd3dadae17b7edf9cb3789
José Leyva <jrleyva41096@gmail.com>
2022-11-12 22:20:36 -0500

    right clicked and formatted code on all files

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const path = require ('path')
+const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT||3001
+const PORT = process.env.PORT || 3001
 
 const api = require('./routes/index.js');
 app.use(express.json());
-app.use ('/api', api);
+app.use('/api', api);
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>
