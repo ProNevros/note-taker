73fdd3b20f051ce239d4bd7fe23974f928ecdcc2
José Leyva <jrleyva41096@gmail.com>
2022-11-12 21:16:23 -0500

    wrote delete route for notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -5,3 +5,14 @@ notes.get('/', (req, res) => {
     readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id != noteId);
+            writeFile('./db/db.json', result);
+            res.json(`Note ${noteId} was deleted`);
+        });
+});
+
